0fd7b5383323c44dccf29761ff01b6f32805e420
Rodolpholn <rodolpho-crack@hotmail.com>
2025-09-27 12:04:16 -0300

    Resolução final

--- Program.cs
+++ Program.cs
@@ -16,10 +16,11 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine("Olá Tudo bem sua Reserva está marcada");
+Console.WriteLine($"Para {reserva.ObterQuantidadeHospedes()} Hóspedes Por durante {reserva.DiasReservados} dias");
+Console.WriteLine($"O valor total das diárias: {reserva.CalcularValorDiaria()}");
